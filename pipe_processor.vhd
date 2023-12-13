270aa23eebb81a68a653129c3d983d38f7aa1b7f
Matthew Leo <99002669+MattL3o@users.noreply.github.com>
2023-12-13 01:55:52 -0500

    Update pipe_processor.vhd

--- pipe_processor.vhd
+++ pipe_processor.vhd
@@ -2,7 +2,7 @@
 --
 -- Title       : processor
 -- Design      : multimedia_ALU
--- Author      : ageorge0368@gmail.com
+-- Author      : Matt and Alan
 -- Company     : Stony Brook University
 --
 -------------------------------------------------------------------------------
